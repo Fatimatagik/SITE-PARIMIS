3e01f62e93602639949ad45a39e9082ec1eac9fd
Fatimata Gikineh <fatimatagik@hotmail.fr>
2022-02-20 05:04:40 +0100

    actualisation des pages

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ let date = document.getElementById('date');
 let message = document.getElementById('message');
 
 let regex_email = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+let regex_telephone = /^\d{4}\-\d{1,2}\-\d{1,2}$/;
 let regex_date = /^\d{4}\-\d{1,2}\-\d{1,2}$/;
 let regex_datearrivee = /^\d{4}\-\d{1,2}\-\d{1,2}$/;
 let regex_datedepart = /^\d{4}\-\d{1,2}\-\d{1,2}$/;
@@ -30,13 +31,44 @@ console.log(button)
         }
         else if(nom.value.length <10 || nom.value.length >15){
             document.getElementById('erreur_nom').innerHTML =
-            `<p class= 'erreur'> veuillez saisir votre nom</p>`
+            `<p class= 'erreur'> Veuillez saisir votre nom</p>`
         }
 
-
+        document.getElementById('erreur_prenom').innerHTML=''
         if(prenom.value===""){
             document.getElementById('erreur_prenom').innerHTML=`
             <p class= 'erreur'> veuillez saisir votre prénom</p>`
         }
+        else if(prenom.value.length <10||prenom.value.length> 15){
+            document.getElementById('erreur_prenom').innerHTML=
+            `<p class='erreur'> Veuillez saisir votre prénom </p>`
+        }
+
+        document.getElementById('erreur_mail').innerHTML=''
+        if(!regex_email.test(email.value)){
+            document.getElementById('erreur_mail').innerHTML=`
+            <p class= 'erreur'> veuillez saisir votre mail</p>`
+        }
+        
+        document.getElementById('erreur_telephone').innerHTML=''
+        if(prenom.value===""){
+            document.getElementById('erreur_telephone').innerHTML=`
+            <p class= 'erreur'> veuillez saisir votre numéro de téléphone </p>`
+        }
+        
+        
+
+    
+        
+        
+        
+
+
+
+
+
+
+
+
     })
 
